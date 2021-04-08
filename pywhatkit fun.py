51f444cddd6ee32be81ca7808cd938813f5284ff
suganya27302 <77053019+suganya27302@users.noreply.github.com>
2021-04-08 22:55:42 +0530

    Add files via upload
    Pywhatkit to play utube etc..

---
+++ pywhatkit fun.py
@@ -0,0 +1,8 @@
+import pywhatkit as pw
+#pw.info("python",lines=3)
+#pw.image_to_ascii_art("flower.jfif","sample4.text")
+#pw.text_to_handwriting("suganya i like you you will succeed in your life and reach heights",rgb=[0,0,0])
+#pw.search("dbms")
+#pw.help()
+#pw.showHistory()
+pw.playonyt("thuli thuli")
